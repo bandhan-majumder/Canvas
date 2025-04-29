4f968d405fb19ed504ebe292b395bffe41acd483
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-04-29 08:46:02 +0530

    jwt added

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,28 @@
 import { WebSocketServer } from "ws";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 const wss = new WebSocketServer({ port: 3002 });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, request) => {
+    const url = request.url;
+
+    if (!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') ??  "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if ( typeof decoded === "string"){
+        return;
+    }
+
+    if(!decoded || !decoded.userId){ // !(decoded as JwtPayload)
+        ws.close(); // close the connection
+        return;
+    }
+
     ws.on('message', (data) => {
         ws.send("ping")
     })
