d6aea26fdab2ed846c07ff2d8bca3b68dc889f7e
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-06-22 03:53:04 +0530

    env changes

--- apps/web/config.ts
+++ apps/web/config.ts
@@ -1,2 +1,1 @@
-export const HTTP_BACKEND_URL = "http://localhost:8080";
-export const WS_BACKEND_URL = "ws://localhost:8081";
+export const WS_BACKEND_URL = process.env.NEXT_PUBLIC_WS_BACKEND_URL || "ws://localhost:3000/ws";
