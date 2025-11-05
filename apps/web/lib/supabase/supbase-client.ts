5ff98eb75c12768cf44b2f0e6cf5e9b4e196c4c5
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-11-05 07:53:12 +0530

    add types to database

--- apps/web/lib/supabase/supbase-client.ts
+++ apps/web/lib/supabase/supbase-client.ts
@@ -1,7 +1,8 @@
+import { Database } from '@/database.types'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
+export const supabase = createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY! || "NONE",
 )
